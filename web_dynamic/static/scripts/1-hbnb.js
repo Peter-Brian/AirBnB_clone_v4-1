2d00e1a0d315fe00375942dccc511ff2fbea7619
jhuang10123 <133@holbertonschool.com>
2018-01-16 21:27:32 +0000

    update setup

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+
+$(":checkbox").on("change", function() {
+  if (this.checked) {
+//append amenity id to list
+//      let list = [];
+  alert("testing123");
+    } else {
+//remove amenity id 
+//      list.pop({{amty.id}});
+      };
+//  $('DIV#H4').text(list);
+});
