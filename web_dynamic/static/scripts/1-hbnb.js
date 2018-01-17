629008049888d3e866f970fb64a99c61b9972a89
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2018-01-17 09:28:04 +0000

    random single quote

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,7 +2,7 @@ let amen = [];
 $(document).ready(function () {
   $('INPUT[type=checkbox]').click(function () {
     if (this.checked) {
-      console.log('$(this).attr('data-name'))
+      console.log($(this).attr('data-name'))
       amen.push($(this).attr('data-name'));
     } else {
       amen.pop($(this).attr('data-name'));
