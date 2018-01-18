c1360f2d64095431586435f58d25e00daab788fe
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2018-01-18 04:50:34 +0000

    added 1-hbnb.js code to other files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,8 +1,19 @@
-$.getJSON('http://0.0.0.0:5001/api/v1/status/', function(data) {
+let amen = [];
+$(document).ready(function () {
+  $('INPUT[type=checkbox]').click(function () {
+    if (this.checked) {
+      amen.push($(this).attr('data-name'));
+    } else {
+      amen.splice(amen.indexOf($(this).attr('data-name')), 1);
+    }
+    $('.amenities h4').text(amen);
+  });
+});
+
+$.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
   if (data.status === 'OK') {
     $('DIV#api_status').addClass('available');
-    } else {
-      $('DIV#api_status').removeClass('available');
-      }
- 
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
 });
