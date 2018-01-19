c4221efa1c79300392585e7bcc8976747e99e9d8
jhuang10123 <133@holbertonschool.com>
2018-01-19 02:44:27 +0000

    task 4 setup

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,33 +1,17 @@
-let amen = [];
-$(document).ready(function () {
-   $('INPUT[type=checkbox]').click(function () {
-    if (this.checked) {
-      amen.push($(this).data('name'));
-    } else {
-      amen.splice(amen.indexOf($(this).data('name')), 1);
-    }
-    $('.amenities h4').text(amen.join(', '));
-  });
-});
-
 $(function () {
-//  let url = 'http://0.0.0.0:5001/api/v1/places_search/';
-
   $.ajax({
     'type': 'POST',
     'url': 'http://0.0.0.0:5001/api/v1/places_search/',
     'Content-Type': 'application/json',
     'dataType': 'json',
-    'data': {}
-   'success': function() {
-      // Loop through result
-      for (let i = 0; i < data.length; i++ ) {
-	// add <article> to each item and list out components for each item. reference visual layout for previous tasks 
-	$('<article></article>').appendTo($('.places'));
+    'data': {},
+    'success': function (places) {
+      for (let i = 0; i < places.length; i++ ) {
+	let article = '<article> <h2>' + places[i].name + '</h2><div class="price_by_night"><p>' + places[i].price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' +  places[i].max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + places[i].number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + places[i].number_bathrooms + '</p></div></div><div class="description"><p>' + places[i].description + '</p></div> </article>';
+	$('.places').append(article)
       };
+    }
   });
-
 });
 
-//data = {place, place2, place3}
-// want name of place and info underneath 
+
